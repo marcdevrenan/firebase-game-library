115c16b8bd07cd7cc8d89b81c8d476d253f63310
marcdevrenan@gmail.com <marcdevrenan@gmail.com>
2022-09-10 10:16:13 -0300

    add login and password

--- app/src/main/java/br/edu/infnet/firebasegamelibrary/LoginFragment.kt
+++ app/src/main/java/br/edu/infnet/firebasegamelibrary/LoginFragment.kt
@@ -1,19 +1,77 @@
 package br.edu.infnet.firebasegamelibrary
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.edu.infnet.firebasegamelibrary.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+
+        initInteractions()
+    }
+
+    private fun initInteractions() {
+        binding.btnLoginUser.setOnClickListener {
+            validator()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+
+                }
+            }
+    }
+
+    private fun validator() {
+        val email = binding.lblEmail.text.toString().trim()
+        val password = binding.lblPwd.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+                loginUser(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Enter a password", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Enter a email", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/br/edu/infnet/firebasegamelibrary/RegisterFragment.kt
+++ app/src/main/java/br/edu/infnet/firebasegamelibrary/RegisterFragment.kt
@@ -1,18 +1,72 @@
 package br.edu.infnet.firebasegamelibrary
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.edu.infnet.firebasegamelibrary.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+
+        initInteractions()
+    }
+
+    private fun initInteractions() {
+        binding.btnRegisterUser.setOnClickListener {
+            validator()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                } else {
+
+                }
+            }
+    }
+
+    private fun validator() {
+        val email = binding.lblEmail.text.toString().trim()
+        val password = binding.lblPwd.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+                createUser(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Enter a password", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Enter a email", Toast.LENGTH_LONG).show()
+        }
     }
 }
